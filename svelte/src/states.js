ba359712aa616495443d54a085df27e533de955f
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-15 22:11:06 +0330

    pathDelimiter constant created.
    description: it holds \ or / as path delimiter depending on the platform

--- svelte/src/states.js
+++ svelte/src/states.js
@@ -2,3 +2,5 @@ import { writable } from "svelte/store";
 
 // [C]urrent [W]orking [D]irectory path
 export const CWDPath = writable("");
+export const pathDelimiter = 
+    process.platform === "win32" ? "\\" : "/";
