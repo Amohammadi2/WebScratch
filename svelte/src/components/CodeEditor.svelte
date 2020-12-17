35d03c77c4d873fcc718451278ef80843b472844
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-17 22:05:33 +0330

    created: CodeEditor.svelte

---
+++ svelte/src/components/CodeEditor.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { onMount } from "svelte";
+    
+    let textArea;
+    onMount(() => {
+        let editor = CodeMirror.fromTextArea(textArea, {
+            lineNumbers: true,
+            mode: "javascript"
+        });
+        console.log("modes", CodeMirror.modes);
+    });
+</script>
+
+<div class="code-container">
+    <div class="action-bar">
+        <button class="action-btn primary">run</button>
+        <button class="action-btn secondary">save</button>
+    </div>
+    <textarea bind:this={textArea}></textarea>
+</div>
+
+<style lang="scss">
+    @import "./../variables.scss";
+    $action-bar-height: 40px;
+
+    textarea {
+        width: 100%;
+        height: cacl(#{$full}- #{$action-bar-height});
+    }
+    .code-container{
+        .action-bar {
+            height: $action-bar-height;
+            width: calc(#{$full} - #{$canvas-width});
+            position: absolute;
+            top:0;
+            right: 0;
+            left: 0;
+            display: flex;
+            justify-content: left;
+            align-items: center;
+            background-color: rgb(245, 245, 245);
+
+            button {
+                margin-top: 8px;
+                margin-left: 1px;
+                margin-right: 1px;
+                outline: none;
+                border: 0;
+                cursor: pointer;
+
+                &.action-btn{
+                    padding: 4px 8px;
+
+                    &.primary{
+                        background-color: rgb(15, 96, 189);
+                        color: white;
+                    }
+
+                    &.secondary{
+                        background-color: rgb(48, 48, 48);
+                        color: rgb(255, 255, 255);
+                    }
+                }   
+            }
+        }
+    }
+</style>
