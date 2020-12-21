5d173319b990e823357081755b37463db285f822
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-21 21:00:36 +0330

    created: EditorWindow.svelte component.
    details:
    - this component was to place the editor inside it

---
+++ svelte/src/components/EditorWindow.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { isEditorOpened } from "../states";
+    import { fly } from "svelte/transition";
+</script>
+
+{#if $isEditorOpened}
+    <div class="editor-window" transition:fly={{y: -200, duration: 300}}>
+        <slot></slot>
+    </div>
+{/if}
+
+<style lang="scss">
+    .editor-window {
+        position: fixed;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        z-index: 10;
+        background-color: rgb(255, 255, 255);
+    }
+</style>
