0b2a4353581a804470566831d05bd8d5596a173e
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-25 13:34:18 +0330

    created: Notification.svelte

---
+++ svelte/src/components/Notification.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let msg;
+    export let type;
+
+    let colorCodes = {
+        "alert": "red",
+        "success": "green",
+        "info": "white"
+    };
+
+</script>
+
+<div class="notification" style={`background-color: ${colorCodes[type] || "white"};`}>
+    {msg}
+</div>
