874236d3a31ccf7854d2a1805f57e19009dd8b13
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-27 10:53:06 +0330

    updated: App.svelte
    details:
    - bound the game canvas to `gameCanvas` variable
    - added a boolean variable called `isPhysicsEngineRunning`
    - added function `runPhysicsEngine`
    - added an onMount handler for the main component
    - added a play and stop button for the physics engine
    - modified some scss styles

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
+    import { onMount } from "svelte";
     import { flip } from "svelte/animate";
-    import { notifications } from "./states";
+    import { notifications, gameComponents} from "./states";
+    import { GameObject } from "./utils";
     import ProjectDirectory from "./components/ProjectDirectory.svelte";
     import CodeEditor from "./components/CodeEditor.svelte";
     import EditorWindow from "./components/EditorWindow.svelte";
@@ -8,11 +10,70 @@
     import ComponentCreator from "./components/ComponentCreator.svelte";
     import Notification from "./components/Notification.svelte";
 
+    let gameCanvas;
+    let isPhysicsEngineRunning = false;
+
     function closeNotification (pk) {
         notifications.update((value) => {
             return value.filter((val) => (val.pk != pk));
         });
     }
+
+    function runPhysicsEngine() {
+        
+        isPhysicsEngineRunning = !isPhysicsEngineRunning;
+        if (isPhysicsEngineRunning){
+            Matter.Runner.run(EngineRunner, PhysicsEngine);
+        }
+        else {
+            Matter.Runner.stop(EngineRunner);
+        }
+    }
+
+    onMount(() => {
+        console.log(gameCanvas);
+        window.gm = gameComponents;
+        window.GameObject = GameObject;
+        window.PhysicsEngine = Matter.Engine.create();
+        window.Render = Matter.Render.create({
+            element: gameCanvas,
+            engine: PhysicsEngine,
+            options: {
+                wireframes: false,
+                background: "rgb(240, 240, 240)",
+                width: 800,
+                height: 500
+            }
+        });
+        window.EngineRunner = Matter.Runner.create();
+
+        window.onresize = function resizeCanvas() {
+            Render.canvas.width = 650;
+            Render.canvas.height = document.documentElement.clientHeight;
+            console.log("canvas resized to fit the screen");
+
+            return resizeCanvas;
+        }();
+
+        let cons = Matter.MouseConstraint.create(PhysicsEngine, {
+            mouse: Matter.Mouse.create(Render.canvas),
+            constraint: {
+                stiffness: 0.1,
+                render: {
+                    visible: false,
+                }
+            }
+        });
+
+        let ground = Matter.Bodies.rectangle(325,600, 650,50, {
+            isStatic: true
+        });
+        let player = Matter.Bodies.rectangle(50,100, 50, 50);
+
+        Matter.World.add(PhysicsEngine.world, [cons, ground, player]);
+
+        Matter.Render.run(Render);
+    });
 </script>
 
 <ComponentCreator />
@@ -37,9 +98,10 @@
         <ComponentsEditor />
     </div>
     <div class="col-right">
-        <div class="game-canvas-container">
-            <canvas id="game-canvas"></canvas>
-        </div>
+        <button class={isPhysicsEngineRunning && "active"} id="play-btn" on:click={runPhysicsEngine}>
+            play
+        </button>
+        <div class="game-canvas-container" bind:this={gameCanvas}></div>
         <!-- shows the project files -->
     </div>
 </div>
@@ -66,16 +128,21 @@
         .col-right {
             float: left;
             width: $canvas-width;
+            position: relative;
             
             .game-canvas-container {
                 width: $full;
                 height: $canvas-height;
                 margin: 0; padding: 0;
+            }
 
-                #game-canvas {
-                    background-color: rgb(245, 245, 245);
-                    width: $canvas-width;
-                    height: $canvas-height;
+            #play-btn {
+                position: absolute;
+                top: 20px;
+                right: 20px;
+
+                &.active {
+                    background-color: rgb(0, 233, 97);
                 }
             }
         }
