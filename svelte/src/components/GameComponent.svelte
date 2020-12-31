1f47615d098bf078e6930321e4cc9b4f880d9a18
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-31 12:58:52 +0330

    created: GameComponent.svelte
    details:
    - added the functionality to select a component and show its details
    - added some futher stylings

---
+++ svelte/src/components/GameComponent.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { activeComponent } from "../states";
+    export let component;
+
+    function setActiveComponent(event) {
+        activeComponent.set(component);
+    }
+</script>
+
+<div class="game-component {($activeComponent == component) && 'active'}" on:click={setActiveComponent}>
+    <span class="material-icon">
+        <img src="./icons/component.svg" alt={"component"} />
+    </span>
+    <span class="comp-name">
+        {component.body.label}
+    </span>
+</div>
+
+<style lang="scss">
+    @import "../utils.scss";
+
+    .game-component {
+        cursor: pointer;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 5px 4px;
+        transition: all .09s ease-out;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        &:hover {
+            background-color: rgb(206, 237, 252);
+        }
+
+        &.active {
+            background-color: rgb(0, 119, 179);
+            color: white;
+        }
+
+        .comp-name {
+            margin: 5px 8px;
+        }
+    }
+</style>
