63b1acdd359bc548029774fbea427c8e6140cc9d
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-12-31 12:59:58 +0330

    updated: ComponentsEditor.svelte
    details:
    - used GameComponent for representation on the editor
    - moved the stylings for scrollbar to global.css

--- svelte/src/components/ComponentsEditor.svelte
+++ svelte/src/components/ComponentsEditor.svelte
@@ -1,6 +1,7 @@
 <script>
     import { CWDPath, gameComponents, isComponentCreatorOpened } from "../states";
     import { NotificationAPI } from "../utils";
+    import GameComponent from "./GameComponent.svelte";
     function openComponentCreator(event) {
         if (!$CWDPath) {
             NotificationAPI.add("please open project directory first", "alert");
@@ -19,7 +20,7 @@
     </div>
     <div class="items">
         {#each $gameComponents as component}
-            <div class="item">{component.label}</div>
+            <GameComponent component={component}/>
         {/each}
     </div>
 </div>
@@ -32,27 +33,6 @@
         height: 50vh;
         background-color: rgb(250, 250, 250);
         overflow-y: scroll;
-
-        &::-webkit-scrollbar {
-            width: 8px;
-            margin-right: 10px;
-        }
-
-        /* Track */
-        &::-webkit-scrollbar-track {
-            background: #f1f1f1;
-            padding: 3px;
-        }
-
-        /* Handle */
-        &::-webkit-scrollbar-thumb {
-            background: rgb(21, 90, 194);
-        }
-
-        /* Handle on hover */
-        &::-webkit-scrollbar-thumb:hover {
-            background: rgb(12, 39, 80);
-        }
 
         .header {
             width: $full;
