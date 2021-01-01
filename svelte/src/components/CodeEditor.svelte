29632bd50e9a14a7395708bc1c078b83c230ec84
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-01-01 18:25:17 +0330

    updated: CodeEditor.svelte
    details:
    - added the name of the file at the top of the page
    - added saving functionality
    - added `saveContents` function to save the current content to files
    - added some transitions and hover effects to the action-bar buttons

--- svelte/src/components/CodeEditor.svelte
+++ svelte/src/components/CodeEditor.svelte
@@ -1,16 +1,38 @@
 <script>
     import { onMount } from "svelte";
     import { codeEditorContents, isEditorOpened } from "../states";
+    import { NotificationAPI, SystemFile } from "../utils";
     
     let textArea;
     let editor;
 
+    let path, name, content;
+
     function updateTextArea(contents) {
-        editor.getDoc().setValue(contents);
+        if (!contents) return;
+        let [file_contents, file_path, file_name] = contents;
+        path = file_path;
+        name = file_name;
+        content = file_contents
+        editor.getDoc().setValue(file_contents);
     }
 
     function closeEditor(event) {
-        isEditorOpened.set(false);
+        let currentValue = editor.getDoc().getValue();
+        let previousValue = content;
+        console.log("current", currentValue);
+        console.log("pre: ", previousValue);
+        let message = "you haven't saved the file. do you want to close it anyway";
+        if (currentValue == previousValue || confirm(message))
+            isEditorOpened.set(false);
+    }
+
+    function saveContents() {
+        let file = new SystemFile(path);
+        let newContent = editor.getDoc().getValue();
+        file.write(newContent);
+        content = newContent;
+        NotificationAPI.add("file saved successfuly", "success")
     }
 
     onMount(() => {
@@ -29,7 +51,8 @@
     <div class="action-bar">
         <button class="action-btn alert" on:click={closeEditor}>&times;</button>
         <button class="action-btn primary">run</button>
-        <button class="action-btn secondary">save</button>
+        <button class="action-btn secondary" on:click={saveContents}>save</button>
+        <span class="action-btn" style="margin-left: 10px;">{name}</span>
     </div>
     <textarea bind:this={textArea}></textarea>
 </div>
@@ -65,15 +88,32 @@
 
                 &.action-btn{
                     padding: 4px 8px;
+                    transition: all .09s ease-in;
 
                     &.primary{
-                        background-color: rgb(15, 96, 189);
+                        background-color: rgb(26, 117, 221);
                         color: white;
+
+                        &:active {
+                            background-color: rgb(18, 79, 148);
+                        }
+
+                        &:hover {
+                            box-shadow: 0px 0px 3px rgb(29, 130, 245);
+                        }
                     }
 
                     &.secondary{
-                        background-color: rgb(48, 48, 48);
+                        background-color: rgb(63, 63, 63);
                         color: rgb(255, 255, 255);
+
+                        &:active {
+                            background-color: rgb(22, 22, 22);
+                        }
+
+                        &:hover {
+                            box-shadow: 0px 0px 3px rgb(75, 75, 75);
+                        }
                     }
 
                     &.alert {
