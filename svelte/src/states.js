317259669bd36287199ce7fbeabdc48f14284adf
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-01-01 18:21:11 +0330

    updated: states.js
    details:
    - changed the initial contents of `codeEditorContents` to an empty array
    - removed hard-coded component objects

--- svelte/src/states.js
+++ svelte/src/states.js
@@ -7,40 +7,12 @@ export const pathDelimiter =
 export const supportedFileFormats = [
     "json", "js"
 ];
-export const codeEditorContents = writable("");
+export const codeEditorContents = writable([]);
 export const isEditorOpened = writable(false);
 export const gameComponents = writable([]);
 export const isComponentCreatorOpened = writable(false);
 export const notifications = writable([]);
 export const activeComponent = writable(null);
 export const isPhysicsEngineRunning = writable(false);
-export const componentsInitialStates = writable([
-    {
-        mode: "body",
-        type: "rectangle",
-        width: 50,
-        height: 50,
-        x: 100,
-        y: 200,
-        options: {
-            label: "Ilia Chomposak",
-            render: {
-                fillStyle: "red",
-            },
-        },
-    },
-    {
-        mode: "body",
-        type: "rectangle",
-        width: 50,
-        height: 50,
-        x: 200,
-        y: 200,
-        options: {
-            label: "Arshia Chomposak",
-            render: {
-                fillStyle: "green",
-            },
-        },
-    },
-]);
+export const projectFileName = "project.components.json";
+export const componentsInitialStates = writable([]);
